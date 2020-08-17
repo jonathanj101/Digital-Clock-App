a46d8594d6d409cf19a28b5abf0b42e769a130fc
jonathanj101 <jonatjimen@gmail.com>
2020-08-16 23:50:08 -0400

    updated js files, now code looks more clean for readability
    updated js files, now code looks more clean for readability

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-let date = new Date();
+function showTime() {
+    let date = new Date();
 
-let h = date.getHours() % 12 || 12;
-let m = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
-let s = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    let clock = document.querySelector("#clock");
+    let seconds = document.querySelector("#clock-seconds");
+    let time = document.querySelector("#day_night");
 
-let dateFullTime = h + " : " + m + " : " + s;
+    let h = date.getHours() % 12 || 12;
+    let m = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let s = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
-document.querySelector("#clock").innerHTML = dateFullTime;
+    let dateTime = h + " : " + m;
+    let day_Night = h < 12 ? "PM" : "AM";
+
+    clock.textContent = dateTime;
+    seconds.textContent = s;
+    time.textContent = day_Night;
+}
+setInterval(() => {
+    showTime();
+}, 1000);
