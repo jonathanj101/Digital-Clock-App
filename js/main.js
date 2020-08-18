fdae4342f7ea6740adc64cbf157fbf493b311f2f
jonathanj101 <jonatjimen@gmail.com>
2020-08-17 23:14:00 -0400

    folder set-up, organized!
    folder set-up, organized!

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function showTime() {
+    let date = new Date();
+
+    let clock = document.querySelector("#clock");
+    let seconds = document.querySelector("#clock-seconds");
+    let time = document.querySelector("#day_night");
+    let militaryTime = document.querySelector("#military_time")
+
+
+    let h = date.getHours() % 12 || 12;
+    let m = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let s = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+    let dateTime = h + " : " + m;
+    let day_Night = h < 12 ? "PM" : "AM";
+
+    clock.textContent = dateTime;
+    seconds.textContent = s;
+    time.textContent = day_Night;
+}
+
+
+
+setInterval(() => {
+    showTime()
+}, 100);
